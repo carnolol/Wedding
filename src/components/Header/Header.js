f07f6e54076a8dcff891ed71d62049840f92f954
Mike Chadwick <Harakili@gmail.com>
2020-08-22 23:42:18 -0600

    skeleton done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+function Header(props) {
+        return (
+            <div>
+                HEADER.js
+            </div>
+        )
+}
+
+export default Header
---
+++ src/components/Slideshow/Slideshow.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+function Slideshow(props){
+        return (
+            <div>
+                Slideshow.js
+            </div>
+        )
+}
+
+export default Slideshow
