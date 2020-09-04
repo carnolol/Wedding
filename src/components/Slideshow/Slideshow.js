284a1b570130bac4d3d2c61c2cd27fe8efc6da9e
Mike Chadwick <Harakili@gmail.com>
2020-09-04 15:17:32 -0600

    working on slider styling and function of changing slider speeds

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -9,7 +9,7 @@ import './Slideshow.css'
 function Slideshow(props) {
 
     const [pictures, setPictures] = useState([])
-    const [speedOfCarousel, setSpeedOCarousel] = useState(750)
+    let [speedOfCarousel, setSpeedOCarousel] = useState(2500)
 
     useEffect(() => {
         axios
@@ -18,27 +18,37 @@ function Slideshow(props) {
                 setPictures(res.data)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [speedOfCarousel])
+
+        // dots: true,
+        // infinite: true,
+        // autoplaySpeed: 4300,
+        // fade: true,
+        // autoplay: true,
+        // speed: 3000,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // arrows: true,
 
     const settings = {
-        autoplaySpeed: 1500,
+        fade: true, 
+        autoplay: true,
+        autoplaySpeed: speedOfCarousel,
+        // autoplaySpeed is the setting of how long the picture stays visible
         arrows: true,
         infinite: true,
         swipeToSlide: true,
         speed: speedOfCarousel,
+        // speed is the setting of how long it takes to transition or fade between each picture
         slidesToShow: 1,
         slidesToScroll: 1,
     }
 
     let weddingPictures = pictures.map(picture => {
         return (
-            <div className='slider-container'
-                key={picture.id}>
-                    <img className='wedding-picture'
-                        alt='Mike and Claire'
-                        src={picture.img} />
-                    <p>{picture.id}</p>
-            </div>
+                <img className='wedding-picture'
+                    alt='Mike and Claire'
+                    src={picture.img} />
         )
     })
 
@@ -47,14 +57,27 @@ function Slideshow(props) {
 
             <div className='mike-and-claire'>
 
-                <h1>Celebrating Michael and Claire</h1>
-                <h1>A love 7 years in the making</h1>
+                <h1 className='slideshow-h1'>Celebrating Michael and Claire</h1>
+                <h1 className='slideshow-h1'>A love 7 years in the making</h1>
 
             </div>
 
-            <Slider {...settings} style={{height: '50%', width: '80%'}}>
-                {weddingPictures}
-            </Slider>
+            <h1 className='slideshow-h1'>From the beginning of our love till now</h1>
+
+            <div className='slider-container'>
+                <Slider {...settings} style={{width: '90%'}}>
+                    {weddingPictures}
+                </Slider>
+            </div>
+            <div className='slider-btn-container'>
+
+                <button className='slider-buttons'
+                    onClick={() => setSpeedOCarousel(speedOfCarousel += 750)}>Slow Down</button>
+
+                <button className='slider-buttons'
+                    onClick={() => setSpeedOCarousel( speedOfCarousel -= 750)}>Speed up</button>
+
+            </div>
         </div>
     )
 }
