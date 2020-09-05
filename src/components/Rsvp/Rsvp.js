02603f21bb732517953c90b5cd34cf309496d53c
Mike Chadwick <Harakili@gmail.com>
2020-09-05 16:48:04 -0600

    working on front end sending emails now

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -1,13 +1,41 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import {connect} from 'react-redux'
+import TextField from '@material-ui/core/TextField'
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
+import swal from 'sweetalert'
 import './Rsvp.css'
 
 function Rsvp(props){
+
+    const [message, setMessage] = useState('')
+    const [numberOfGuests, setNumberOfGuests] = useState(0)
+    const [requiresLodging, setRequiresLodging] = useState('')
+
+    const sendRsvpEmail = () => {
+        const body = {
+            first_name: props.first_name,
+            last_name: props.last_name,
+            email: props.email,
+            numberOfGuests: numberOfGuests,
+            requiresLodging: requiresLodging
+        }
+        axios.post('/wedding/rsvp', body)
+    }
+
+    const handleReset = () => {
+        setMessage('')
+        setNumberOfGuests(0)
+        setRequiresLodging('')
+    }
+
     return(
         <div className='master-rsvp-div'>
 
             <h1 className='rsvp-h1'>Please RSVP to our wedding!</h1>
 
             <div className='RSVP-container'>
+                
                 <p>Please be sure to check your spam folder for the RSVP E-mail!</p>
             </div>
 
@@ -15,4 +43,6 @@ function Rsvp(props){
     )
 }
 
-export default Rsvp
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps, null)(Rsvp)
